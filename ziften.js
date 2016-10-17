872bbf8b8a4ce99b766dc6e66264199cece39a11
Michael Ritsema <michael.ritsema@ziften.com>
2016-10-17 10:36:04 -0500

    screen recording

--- ziften.js
+++ ziften.js
@@ -1,9 +1,7 @@
-mdc.inited = function() {
-    mermaid.sequenceConfig = {
+    global.mermaid.sequenceConfig = {
         "mirrorActors": false,
         "diagramMarginX":25,
         "diagramMarginY":10,
         "activationWidth": 20,
         "height": 40
     };
-});
